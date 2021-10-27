68b240b0762aab29a5bf50743297e1c7e69a6078
IreneKibui <92978177+IreneKibui@users.noreply.github.com>
2021-10-27 21:14:01 +0300

    update main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int num1, num2;
+    int sum, diff, pro, mod;
+    float quot;
+
+    //assignment
+    num1 = 200;
+    num2 = 56;
+
+    //calculation
+    sum = num1 + num2;
+    diff = num1 - num2;
+    pro = num1 * num2;
+    quot = (float)num1 / num2;
+    mod = num1 % num2;
+
+    //output
+    printf("Sum: %i + %i = %i \n",num1, num2, sum);
+    printf("Diffrence: %i - %i = %i \n",num1, num2, diff);
+    printf("Product: %i * %i = %i \n",num1, num2, pro);
+    printf("quatient: %i / %i = %f \n",num1, num2, quot);
+    printf("Remainder: %i mod %i = %i \n",num1, num2, mod);
+
+    return 0;
+}
